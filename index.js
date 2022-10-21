81302b5e488e231d31bd7767edb4622b88eef3c5
Rogério de Sá <rogeriodesaf@gmail.com>
2022-10-21 19:24:25 -0300

    Primeira rota criada com POST

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const app = express()
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
+
+app.post('/createProduct',(req,res)=>{
+    const name = req.body.name
+    const price = req.body.price
+
+    console.log(name,price)
+    res.json({message:`O produto ${name} foi enviado com sucesso!`})
+})
+
 app.get('/',(req,res)=>{
     res.json({message:'Primeira rota criada com sucesso!'})
 })
